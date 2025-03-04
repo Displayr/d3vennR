bf9de2e203d63e066950fbd09742eb601cd64115
Bruce Tan <bruce.tan@displayr.com>
2025-03-04 17:49:19 +1300

    DPI-2845 Pack d3vennR in nix

---
+++ package.nix
@@ -0,0 +1,13 @@
+{ pkgs ? import <nixpkgs> {}, displayrUtils }:
+
+pkgs.rPackages.buildRPackage {
+  name = "d3vennR";
+  version = displayrUtils.extractRVersion (builtins.readFile ./DESCRIPTION); 
+  src = ./.;
+  description = ''Make interactive d3.js Venn/Euler diagrams in R with the convenience and infrastructure
+              of an htmlwidget.'';
+  propagatedBuildInputs = with pkgs.rPackages; [ 
+    htmlwidgets
+ ];
+
+}
